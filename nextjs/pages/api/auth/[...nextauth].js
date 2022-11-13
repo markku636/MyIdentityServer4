6280ebfd881e3c6f18b3cfc8bcf980a3df84db43
Mark.K <a4756830@gmail.com>
2022-11-13 22:41:52 +0800

    update

---
+++ nextjs/pages/api/auth/[...nextauth].js
@@ -0,0 +1,58 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    Providers.IdentityServer4({
+      id: "identity-server4",
+      name: "IdentityServer4",
+      scope: "openid profile api1", // Allowed Scopes
+      domain: process.env.IdentityServer4_Domain,
+      clientId: process.env.IdentityServer4_CLIENT_ID,
+      clientSecret: process.env.IdentityServer4_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    // async signIn(user, account, metadata) {
+    //   debugger;
+    //   if (account.provider === "github") {
+    //     const githubUser = {
+    //       id: metadata.id,
+    //       login: metadata.login,
+    //       name: metadata.name,
+    //       avatar: user.image,
+    //     };
+
+    //     user.accessToken = await getTokenFromYourAPIServer(
+    //       "github",
+    //       githubUser
+    //     );
+    //     return true;
+    //   }
+
+    //   return false;
+    // },
+    /**
+     * @param  {object} session      Session object
+     * @param  {object} token        User object    (if using database sessions)
+     *                               JSON Web Token (if not using database sessions)
+     * @return {object}              Session that will be returned to the client
+     */
+    async session(session, token) {
+      // Add property to session, like an access_token from a provider.
+
+      session.accessToken = token.accessToken;
+      return session;
+    },
+
+    async jwt(token, user, account, profile, isNewUser) {
+      // Add access_token to the token right after signin
+
+      if (account?.accessToken) {
+        token.accessToken = account.accessToken;
+      }
+      return token;
+    },
+  },
+});
---
+++ nextjs/pages/index.js
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/client";
+export default function Home() {
+  const [session, loading] = useSession();
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+        {!session && (
+          <>
+            Not signed in <br />
+            <button
+              className="text-2xl  font-bold p-10"
+              onClick={() => signIn()}
+            >
+              Sign in
+            </button>
+
+            <button
+              className="text-2xl  font-bold p-10"
+              onClick={() => signIn('google', { callbackUrl: 'http://localhost:3000/bar' })}
+            >
+              Sign in Google
+            </button>
+          </>
+        )}
+        {session && (
+          <>
+            Signed in as {session.user.email} <br />
+            {console.log(session)}
+            {JSON.stringify(session)}
+            <button
+              className="text-2xl font-bold p-10"
+              onClick={() => signOut()}
+            >
+              Sign out
+            </button>
+          </>
+        )}
+      </main>
+    </div>
+  );
+}
